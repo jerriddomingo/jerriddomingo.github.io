09fba70ac608d1984a732a8520e3abcf4ce778ba
Jerrid Domingo <jerridjdomingo@gmail.com>
2020-06-19 23:46:01 -0500

    master

---
+++ js/about.fa9c9133.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,e,n){"use strict";n.r(e);var a=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},o=[function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{staticClass:"about"},[n("h1",[t._v("About Me")]),n("p",[t._v(" I don't have any information yet.")])])}],u=n("2877"),r={},s=Object(u["a"])(r,a,o,!1,null,null,null);e["default"]=s.exports}}]);
+//# sourceMappingURL=about.fa9c9133.js.map
